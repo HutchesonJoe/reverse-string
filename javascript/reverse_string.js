8b658d9fb5d3c02996891190717d2482bd6f5548
Joe <hutcheson2001@yahoo.com>
2022-02-26 20:27:13 -0500

    Completed assignment

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,9 +1,20 @@
 function reverseString(str) {
-  // type your code here
+  const revArray = []
+  const splitString = str.split('');
+  for (const letter of splitString){
+    revArray.unshift(letter)
+  }
+  const reversedString = revArray.join('');
+  return reversedString;
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: '?3uhHs'");
+  console.log("=>", reverseString("sHhu3?"));
+
+  console.log("");
+
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
@@ -15,5 +26,8 @@ if (require.main === module) {
 
 module.exports = reverseString;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+// PROBLEM - recieve a string, and reverse it. Anything submitted as a string will be returned backward.
+  //this fuction takes in a string
+  //splits the string into each character, returning an array of characters
+  //removes the last letter from this array and places it at the beginning of a new array
+  //joins and returns the new array
